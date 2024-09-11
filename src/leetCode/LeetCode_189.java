8f68d70479bc7c76cf12db5332926530bc9a69d5
DucNguyen <macos@192.168.1.5>
2024-09-11 07:59:13 +0700

    commit leetcode_189_v1

--- src/leetCode/LeetCode_189.java
+++ src/leetCode/LeetCode_189.java
@@ -1,8 +1,39 @@
 package leetCode;
 
+import java.util.Arrays;
+
 public class LeetCode_189 {
 	
-	public void rotate(int[] nums, int k) {
-        
+//	public static void rotate(int[] nums, int k) {
+//        int n = nums.length;
+//		for(int i = 0; i < k; i++) {
+//			int[] numsCp = new int[n];
+//			 numsCp[0] = nums[n - 1];
+//			for(int j = 1; j < n; j++) {
+//				numsCp[j] = nums[j - 1];
+//			}
+//			nums = numsCp;
+//		}
+//		for(int i = 0; i < n ;i++) {
+//			nums[i] = nums[i];
+//		}
+//		System.out.println(Arrays.toString(nums));
+//    }
+	
+	public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] numsCp = new int[n];
+		for(int i = 0; i < n ; i ++) {
+			int new_idx = (i + k) % n;
+			numsCp[new_idx] = nums[i];
+		}
+		for(int i = 0; i < n ; i ++) {
+			nums[i] = numsCp[i];
+		}
     }
+	
+	public static void main(String[] args) {
+		int nums[] = {1,2,3,4,5,6,7};
+		rotate(nums,3);
+	}
 }
